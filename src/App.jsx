56e0be6cc3497712e3bde8fef64e5c135ec706de
yusufkaraasln <yusufkaraasln@gmail.com>
2024-06-07 07:09:13 +0300

    changes io ip address

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,7 @@ function App() {
   const [auctionDetails, setAuctionDetails] = useState({});
 
   useEffect(() => {
-    const socket = io("34.68.138.79", {
-    });
+    const socket = io("https://acuction-api-demo.onrender.com/", );
     socketRef.current = socket;
 
     const onConnected = () => setIsConnected(true);
